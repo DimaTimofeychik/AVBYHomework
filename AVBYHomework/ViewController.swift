147dd7fd2beda829d2b1b6082ec20786ae1bfef5
dimatimofeychik <dimatimofeychik@gmail.com>
2023-12-01 14:13:18 +0300

    first commit

---
+++ AVBYHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVBYHomework
+//
+//  Created by Dima on 1.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
